28da1c52d1c1e29bcf286226f6703d7ecac57cf9
Darklaw-game <darklaw.game@gmail.com>
2025-03-11 18:08:57 -0500

    Update SlapBattles.lua

--- SlapBattles.lua
+++ SlapBattles.lua
@@ -1,46 +1,206 @@
-Gloves = 0
-for i,v in ipairs(workspace.Lobby["GloveStands"]:GetChildren()) do
-if v then
-Gloves = Gloves + 1
-end
-end
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+    Name = "Slap Battles Hub",
+    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+    LoadingTitle = "Rayfield Interface Suite",
+    LoadingSubtitle = "by Sirius",
+    Theme = "Ocean", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+ 
+    DisableRayfieldPrompts = false,
+    DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+ 
+    ConfigurationSaving = {
+       Enabled = true,
+       FolderName = nil, -- Create a custom folder for your hub/game
+       FileName = "Big Hub"
+    },
+ 
+    Discord = {
+       Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+       Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+    },
+ 
+    KeySystem = false, -- Set this to true to use our key system
+    KeySettings = {
+       Title = "Untitled",
+       Subtitle = "Key System",
+       Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+       Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+    }
+ })
 
-local GameName = "recall cheat sheet"
 
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({IntroText = "recall cheat sheet yippe", IntroIcon = "rbxassetid://15315284749",Name = GameName, HidePremium = false, SaveConfig = true, ConfigFolder = "Tutorial"})
-
-OrionLib:MakeNotification({Name = "Warning",Content = "Use at your own risk.",Image = "rbxassetid://7733658504",Time = 5})
-
-local Tab = Window:MakeTab({
-	Name = "cheat sheet",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
+ local badgesTab = Window:CreateTab("badges", 4483362458) -- Title, Image
+ 
 
 
-Tab:AddParagraph("When was Slap Battles first published?","16/2/2021")
-Tab:AddParagraph("What glove costs 9750 slaps?","Coil")
-Tab:AddParagraph("What is the chance of getting bob from Replica?","1/7500")
-Tab:AddParagraph("How many gloves stands are in Slap Battles right now?",""..Gloves.."")
-Tab:AddParagraph("What is the badge name for the Glitch glove?","01010010 01010101 01001110")
-Tab:AddParagraph("If 1=3 2=3 3=5 4=4 5=4 6=?","3")
-Tab:AddParagraph("Whom ultimately controls this realm?","Tencell")
-Tab:AddParagraph("How many slaps do you have?",""..game.Players.LocalPlayer.leaderstats.Slaps.Value.."")
-Tab:AddParagraph("How many players were present in this server when you were sent here?",""..#game.Players:GetPlayers().." / "..game.Players.MaxPlayers.."")
-Tab:AddParagraph("What color is your name in the Roblox Chat?",""..require(game:GetService("Chat")):GetPlayerChatColor(game.Players.LocalPlayer).." (may not work)")
-Tab:AddParagraph("How old is your account in days?",""..game.Players.LocalPlayer.AccountAge.."")
-Tab:AddParagraph("What has a bottom at the top?","A leg (You must type in the chat)")
-Tab:AddParagraph("Did you forget that you repressed your memories?","Yes")
-Tab:AddParagraph("Do you know who I am?","A forgotten memory")
-Tab:AddParagraph("How many players currently exist in this realm as of when I started asking this question?"," [ "..#game.Players:GetPlayers().." / "..game.Players.MaxPlayers.." ] (re-execute script to get correct answer)")
-Tab:AddParagraph("What has 4 fingers and one thumb but is not alive?","Type ''Glove'' in chat.")
-Tab:AddParagraph("What glove can't you hit when it isn't there?","Elude")
-Tab:AddParagraph("We will play Simon Says at the end, do you understand?","Yes")
+ local Button = badgesTab:CreateButton({
+    Name = "BypassAntiCheat",
+    Callback = function()
+        loadstring(game:HttpGet("https://github.com/Pro666Pro/BypassAntiCheat/blob/main/main.lua",true))()
+    -- The function that takes place when the button is pressed
+    end,
+ })
 
-Tab:AddParagraph("Simon says if you seek for the reward, dance on top of the entities head.","Dance on top of the clone's head (by typing ''/e dance'') if it says ''Simon says''. Walk through the portal if it doesn't say ''Simon says''")
-Tab:AddParagraph("Simon says walk through the portal to claim your reward","Self-explanatory. Walk through the portal if it says ''Simon says''. Jump off the map if it doesn't say ''Simon says''")
-Tab:AddParagraph("Simon says jump off the map to claim your reward","Self-explanatory. Jump off the map if it says ''Simon says''. Walk through the portal if it doesn't say ''Simon says''")
-Tab:AddParagraph("Simon says if the reward is what you seek, You must dance on top of the Right/Left staircase Crystal.","Dance on top of the Crystals according to the NPC's Perspective by using the command ''/e dance'' in chat. If it says, ''Left Crystal'', You go and Dance on the Right Crystal (Its Left will be your Right and its Right will be your Left). Walk through the portal if it doesn't say ''Simon says''")
-Tab:AddParagraph("Simon says riddle me this and speak it out. What has four fingers and a thumb but isn’t alive?","Type ''a glove'' in chat.")
-Tab:AddParagraph("Simon says riddle me this and speak it out. What has a bottom at the top?","Type ''a leg'' in chat.")
+
+
+
+ print(game.Players.LocalPlayer.leaderstats.Slaps.Value)
+ print(game.Players.LocalPlayer.AccountAge)
+ local Button = badgesTab:CreateButton({
+    Name = "Please click its info",
+    Callback = function()
+        Rayfield:Notify({
+            Title = "Hi",
+            Content = "Use /Console for info of your stats",
+            Duration = 6.5,
+            Image = 4483362458,
+         })
+    -- The function that takes place when the button is pressed
+    end,
+ })
+
+ local Button = badgesTab:CreateButton({
+    Name = "Auto-Get Metaverse",
+    Callback = function()
+            loadstring(game:HttpsGet("https://raw.githubusercontent.com/Pro666Pro/AutoGetMetaverse/refs/heads/main/OpenSource.lua",true))()
+    -- The function that takes place when the button is pressed
+    end,
+ })
+
+ local Button = badgesTab:CreateButton({
+    Name = "Auto-Get Hexagon",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Pro666Pro/AutoGetHexagon/refs/heads/main/OpenSource.lua",true))()
+    -- The function that takes place when the button is pressed
+    end,
+ })
+
+ local Button = badgesTab:CreateButton({
+    Name = "Auto-Get Mouse",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Pro666Pro/AutoGetMouse/refs/heads/main/OpenSource.lua",true))()
+    -- The function that takes place when the button is pressed
+    end,
+ })
+
+ local Button = badgesTab:CreateButton({
+    Name = "Auto-Get Bob",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Pro666Pro/AutoGetBob/refs/heads/main/main.lua",true))()
+    -- The function that takes place when the button is pressed
+    end,
+ })
+
+ local Button = badgesTab:CreateButton({
+    Name = "Auto-Get Untitled Tag",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Pro666Pro/autogettagglove/refs/heads/main/main.lua",true))()
+    -- The function that takes place when the button is pressed
+    end,
+ })
+
+ local Button = badgesTab:CreateButton({
+    Name = "Auto-Get Clock",
+    Callback = function()
+         loadstring(game:HttpGet("https://raw.githubusercontent.com/Pro666Pro/AutoGetClockExtended/refs/heads/main/main.lua",true))()
+    -- The function that takes place when the button is pressed
+    end,
+ })
+
+ local Button = badgesTab:CreateButton({
+    Name = "FishVoodooTrap",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Pro666Pro/fish-voodoo-trap/refs/heads/main/main.lua",true))()
+    -- The function that takes place when the button is pressed
+    end,
+ })
+
+ local GuisTab = Window:CreateTab("Guis", 4483362458) -- Title, Image
+ 
+
+ 
+ local GuisButton = GuisTab:CreateButton({
+    Name = "King Sbeve",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Pro666Pro/KingSbeve/refs/heads/main/main.lua",true))()
+    -- The function that takes place when the button is pressed
+    end,
+ })
+
+
+ local GuisButton = GuisTab:CreateButton({
+    Name = "SlapBattles",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Pro666Pro/SlapBattles/refs/heads/main/main.lua",true))()
+    -- The function that takes place when the button is pressed
+    end,
+ })
+
+ local GuisButton = GuisTab:CreateButton({
+    Name = "MasteryFarm",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Pro666Pro/MasteryFarmGui/refs/heads/main/main.lua",true))()
+    -- The function that takes place when the button is pressed
+    end,
+ })
+
+ local GuisButton = GuisTab:CreateButton({
+    Name = "Slap Farm",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Pro666Pro/slapfarmgui/refs/heads/main/main.lua",true))()
+    -- The function that takes place when the button is pressed
+    end,
+ })
+
+ local GuisButton = GuisTab:CreateButton({
+    Name = "Mastery Farm",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Pro666Pro/MasteryFarmGui/refs/heads/main/main.lua",true))()
+    -- The function that takes place when the button is pressed
+    end,
+ })
+
+ local GuisButton = GuisTab:CreateButton({
+    Name = "Badge Hub",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Pro666Pro/UltimateBadgeHub/refs/heads/main/main.lua",true))()
+    -- The function that takes place when the button is pressed
+    end,
+ })
+
+ local GuisButton = GuisTab:CreateButton({
+    Name = "Nametag",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Pro666Pro/Change_Nametag/refs/heads/main/main.lua",true))()
+    -- The function that takes place when the button is pressed
+    end,
+ })
+
+ local GuisButton = GuisTab:CreateButton({
+    Name = "Recall",
+    Callback = function()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Pro666Pro/recallcheatsheet/refs/heads/main/main.lua",true))()
+    -- The function that takes place when the button is pressed
+    end,
+ })
+
+ local GuisButton = GuisTab:CreateButton({
+    Name = "Bob2",
+    Callback = function()
+        loadstring(game:HttpGet"https://pastefy.app/7qbY4h0Z/raw")()
+    -- The function that takes place when the button is pressed
+    end,
+ })
+
+ local GuisButton = GuisTab:CreateButton({
+    Name = "Guide boss",
+    Callback = function()
+        loadstring(game:HttpGet("https://pastefy.app/Ty7G6BXs/raw"))()
+    -- The function that takes place when the button is pressed
+    end,
+ })
