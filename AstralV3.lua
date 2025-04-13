7e742f1d78d5944524c62d1a1493ad647889860f
Darklaw-game <darklaw.game@gmail.com>
2025-04-13 10:59:45 -0500

    Update AstralV3.lua

--- AstralV3.lua
+++ AstralV3.lua
@@ -38,86 +38,19 @@ local Button = LeftGroupbox:AddButton({
 })
 
 local statsTab = Window:AddTab("Stats", "home") -- Second parameter is the icon name (optional)
-local Label = Groupbox:AddLabel("you have this info")
-local WrappedLabel = Groupbox:AddLabel({
-    Text = "TCheckSlap = Tab:AddLabel("Check Slap [ "..game.Players.LocalPlayer.leaderstats.Slaps.Value.." ]")
-Glove = Tab:AddLabel("You're Using Glove [ "..game.Players.LocalPlayer.leaderstats.Glove.Value.." ]")
-PlateTime = Tab:AddLabel("Plate Time [ "..game.Players.LocalPlayer.PlayerGui.PlateIndicator.TextLabel.Text.." ]")
-Tab:AddParagraph("Game's ID [ "..game.PlaceId.." ]","Server ID [ "..game.JobId.." ]")
-local InfoServer = Tab:AddSection({Name = "Local Player"})
-if game.Players.LocalPlayer.Character:FindFirstChild("rock") then
-WalkspeedYou = Tab:AddLabel("Walk Speed [ Not Walk then rock ]")
-JumppowerYou = Tab:AddLabel("Jump Power [ Not Jump Power then rock ]")
-HealthYou = Tab:AddLabel("Health You [ Not Health then rock ]")
-HipHeightYou = Tab:AddLabel("Hip Height [ Not Hip then rock ]")
-else
-WalkspeedYou = Tab:AddLabel("Walk Speed [ "..game.Players.LocalPlayer.Character.Humanoid.WalkSpeed.." ]")
-JumppowerYou = Tab:AddLabel("Jump Power [ "..game.Players.LocalPlayer.Character.Humanoid.JumpPower.." ]")
-HealthYou = Tab:AddLabel("Health You [ "..game.Players.LocalPlayer.Character.Humanoid.Health.." ]")
-HipHeightYou = Tab:AddLabel("Hip Height [ "..game.Players.LocalPlayer.Character.Humanoid.HipHeight.." ]")
-end
-GravityYou = Tab:AddLabel("Gravity [ "..game.Workspace.Gravity.." ]")
-PositionYou = Tab:AddLabel("Position In Your [ "..tostring(math.round(game.Players.LocalPlayer.Character.HumanoidRootPart.Position.X)..", ".. math.round(game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Y)..", "..math.round(game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Z)).." ]")
-
-local AutoSetInfoServer
-AutoSetInfo = Tab:AddToggle({
-	Name = "Auto Set Info",
-	Default = false,
-	Callback = function(Value)
-_G.AutoSetInfo = Value
-AutoSetInfoServer = game:GetService("RunService").RenderStepped:Connect(function()
-if _G.AutoSetInfo == true then
-CanYouFps:Set("Your Fps [ "..math.floor(workspace:GetRealPhysicsFPS()).." ]")
-ServerPlayer:Set("Player In Server [ "..#game.Players:GetPlayers().." / "..game.Players.MaxPlayers.." ]")
-TimeServer:Set("Server Time [ "..math.floor(workspace.DistributedGameTime / 60 / 60).." Hour | "..math.floor(workspace.DistributedGameTime / 60) - (math.floor(workspace.DistributedGameTime / 60 / 60) * 60).." Minutes | "..math.floor(workspace.DistributedGameTime) - (math.floor(workspace.DistributedGameTime / 60) * 60).." Second ]")
-CanYouPing:Set("Your Ping [ "..game:GetService("Stats").Network.ServerStatsItem["Data Ping"]:GetValueString().." ]")
-AgeAccYou:Set("Your Account Age [ "..game.Players.LocalPlayer.AccountAge.." ]")
-TimeNow:Set("Now Time [ "..os.date("%X").." ]")
-ViewAgeServer:Set("Server's Age [ "..game.Workspace.Lobby.ServerAge.Text.SurfaceGui.TextLabel.Text.." ]")
-PlateTime:Set("Plate Time [ "..game.Players.LocalPlayer.PlayerGui.PlateIndicator.TextLabel.Text.." ]")
-if game.Players.LocalPlayer.Character.Humanoid.Health == 0 then
-ResetTime:Set("Time Spawn [ "..game.Players.RespawnTime.." ]")
-else
-ResetTime:Set("Time Spawn [ Not Dead ]")
-end
-PositionYou:Set("Position In You [ "..tostring(math.round(game.Players.LocalPlayer.Character.HumanoidRootPart.Position.X)..", ".. math.round(game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Y)..", "..math.round(game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Z)).." ]")
-CodeKeypad:Set("Code Keypad [ "..tostring((#game.Players:GetPlayers()) * 25 + 1100 - 7).." ]")
-CheckSlap:Set("Check Slap [ "..game.Players.LocalPlayer.leaderstats.Slaps.Value.." ]")
-Glove:Set("You're Using Glove [ "..game.Players.LocalPlayer.leaderstats.Glove.Value.." ]")
-if game.Players.LocalPlayer.Character:FindFirstChild("rock") then
-WalkspeedYou:Set("Walk Speed [ Not Walk then rock ]")
-JumppowerYou:Set("Jump Power [ Not Jump Power then rock ]")
-HealthYou:Set("Health You [ Not Health then rock ]")
-HipHeightYou:Set("Hip Height [ Not Hip then rock ]")
-else
-WalkspeedYou:Set("Walk Speed [ "..game.Players.LocalPlayer.Character.Humanoid.WalkSpeed.." ]")
-JumppowerYou:Set("Jump Power [ "..game.Players.LocalPlayer.Character.Humanoid.JumpPower.." ]")
-HealthYou:Set("Health You [ "..game.Players.LocalPlayer.Character.Humanoid.Health.." ]")
-HipHeightYou:Set("Hip Height [ "..game.Players.LocalPlayer.Character.Humanoid.HipHeight.." ]")
-end
-GravityYou:Set("Gravity [ "..game.Workspace.Gravity.." ]")
-if not game.Workspace:FindFirstChild("Keypad") then
-KeypadSpawn:Set("Keypad Spawn [ No ]")
-else
-KeypadSpawn:Set("Keypad Spawn [ Yes ]")
-end
-if not game.Workspace:FindFirstChild("Toolbox") then
-ToolboxSpawn:Set("Player Spawn Toolbox [ No ]")
-else
-ToolboxSpawn:Set("Player Spawn Toolbox [ Yes ]")
-end
-if not game.Workspace:FindFirstChild("SiphonOrb") then
-SiphonOrbSpawn:Set("Spawn Siphon Orb [ No ]")
-else
-SiphonOrbSpawn:Set("Spawn Siphon Orb [ Yes ]")
-end
-elseif _G.AutoSetInfo == false then
-AutoSetInfoServer:Disconnect()
-AutoSetInfoServer = nil
-end
-end)
-	end    
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/Darklaw-game/OPS/refs/heads/main/AstralStats.lua",true))()
+local LeftGroupbox = MainTab:AddLeftGroupbox("stats")
+local RightGroupbox = MainTab:AddRightGroupbox(" ")
+local Button = LeftGroupbox:AddButton({
+    Text = "Click Me",
+    Func = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Darklaw-game/OPS/refs/heads/main/AstralStats.lua",true))()
+    end,
+    DoubleClick = true -- Requires double-click for risky actions
 })
 
+local Label = Groupbox:AddLabel("instructions")
+local WrappedLabel = Groupbox:AddLabel({
+    Text = "make sure to use /console in chat to view your statistics"
     DoesWrap = true
 })
