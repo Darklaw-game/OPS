e5940c1abf0e49e2d7f75a6cf84d6935efafde42
Darklaw-game <darklaw.game@gmail.com>
2025-04-13 10:54:20 -0500

    Create AstralStats.lua

---
+++ AstralStats.lua
@@ -0,0 +1,4 @@
+slaps = game.Players.LocalPlayer.leaderstats.Slaps.Value
+glove = game.Players.LocalPlayer.leaderstats.Glove.Value
+print("you have " .. slaps .. "slaps")
+print("you are using the " .. glove .. "glove!"
