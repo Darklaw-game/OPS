cd02348ba7a6723511c96fca51c3dc14fe7622bb
Darklaw-game <darklaw.game@gmail.com>
2025-04-13 11:57:21 -0500

    Update AstralV3.lua

--- AstralV3.lua
+++ AstralV3.lua
@@ -2,7 +2,7 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/deivi
 
 local Window = Library:CreateWindow({
     Title = "Project Astral",
-    Footer = "v1.0.2",
+    Footer = "v1.0.20",
     ToggleKeybind = Enum.KeyCode.RightControl,
     Center = true,
     AutoShow = true
