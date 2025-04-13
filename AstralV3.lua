9db3adfa73dc479ba44266732d83d82a70377068
Darklaw-game <darklaw.game@gmail.com>
2025-04-13 11:37:02 -0500

    Update AstralV3.lua

--- AstralV3.lua
+++ AstralV3.lua
@@ -21,7 +21,7 @@ local Button = LeftGroupbox:AddButton({
     DoubleClick = true -- Requires double-click for risky actions
 })
 
-local Button = LeftGroupbox:AddButton({
+local Button = RightGroupbox:AddButton({
     Text = "Anticheat",
     Func = function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Pro666Pro/BypassAntiCheat/refs/heads/main/main.lua",true))()
@@ -29,7 +29,7 @@ local Button = LeftGroupbox:AddButton({
     DoubleClick = true -- Requires double-click for risky actions
 })
 
-local Button = LeftGroupbox:AddButton({
+local Button = RightGroupbox:AddButton({
     Text = "NickName",
     Func = function()
         while wait() do game.Players.LocalPlayer.Character.Head.Nametag.TextLabel.Text = "Astro" end
@@ -53,4 +53,11 @@ local Button = RightGroupbox:AddButton({
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Darklaw-game/OPS/refs/heads/main/AstralStats.lua",true))()
     end,
     DoubleClick = false -- Requires double-click for risky actions
+})
+local Button = RightGroupbox:AddButton({
+    Text = "Click Me",
+    Func = function()
+        print("Button clicked!")
+    end,
+    DoubleClick = true -- Requires double-click for risky actions
 })
