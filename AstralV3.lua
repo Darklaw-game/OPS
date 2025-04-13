ea0331a28b5988f6ef317f5203fa4b4696febc5e
Darklaw-game <darklaw.game@gmail.com>
2025-04-13 15:38:49 -0500

    Update AstralV3.lua

--- AstralV3.lua
+++ AstralV3.lua
@@ -77,35 +77,5 @@ local BadgeTab = Window:AddTab("Badges", "icon")
 local rightGroupBox = BadgeTab:AddrightGroupbox("Not auto")
 local LeftGroupBox = BadgeTab:AddleftGroupbox("Auto farm/get")
 
-local Button = LeftGroupbox:AddButton({
-    Text = "Elude",
-    Func = function()
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/OpenSourceScripts/refs/heads/main/Gloves/EludeGlove.luau'))()
-    end,
-    DoubleClick = true -- Requires double-click for risky actions
-})
 
-local Button = LeftGroupbox:AddButton({
-    Text = "Counter",
-    Func = function()
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/OpenSourceScripts/refs/heads/main/Gloves/CounterGlove.luau'))()
-    end,
-    DoubleClick = true -- Requires double-click for risky actions
-])
-    
-local Button = LeftGroupbox:AddButton({
-    Text = "Clock",
-    Func = function()
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/OpenSourceScripts/refs/heads/main/Gloves/ClockGlove.luau'))()
-    end,
-    DoubleClick = true -- Requires double-click for risky actions
-})    
-
-local Button = LeftGroupbox:AddButton({
-    Text = "Boxer",
-    Func = function()
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/OpenSourceScripts/refs/heads/main/Gloves/BoxerGlove.luau'))()
-    end,
-    DoubleClick = true -- Requires double-click for risky actions
-})
     
