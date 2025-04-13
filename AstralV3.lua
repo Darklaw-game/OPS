78a755075e57f4fd85e0705c876aadd83de5a8aa
Darklaw-game <darklaw.game@gmail.com>
2025-04-13 11:28:48 -0500

    Update AstralV3.lua

--- AstralV3.lua
+++ AstralV3.lua
@@ -8,7 +8,6 @@ local Window = Library:CreateWindow({
     AutoShow = true
 })
 
-local Stattab = Window:AddTab("stat", "icon")
 local MainTab = Window:AddTab("Main", "home") -- Second parameter is the icon name (optional)
 
 local LeftGroupbox = MainTab:AddLeftGroupbox("hubs")
@@ -36,4 +35,22 @@ local Button = LeftGroupbox:AddButton({
         while wait() do game.Players.LocalPlayer.Character.Head.Nametag.TextLabel.Text = "Astro" end
     end,
     DoubleClick = true -- Requires double-click for risky actions
+})
+
+local Stattab = Window:AddTab("stat", "icon")
+local LeftGroupbox = StatTab:AddLeftGroupbox("Main")
+local RightGroupbox = StatTab:AddRightGroupbox("Informatiormation")
+
+local Label = RightGroupbox:AddLabel("Information")
+local WrappedLabel = RightGroupbox:AddLabel({
+    Text = "Use /console in chat for stats (rn only glove and slaps)",
+    DoesWrap = true
+})
+
+local Button = Groupbox:AddButton({
+    Text = "Stats",
+    Func = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Darklaw-game/OPS/refs/heads/main/AstralStats.lua",true))()
+    end,
+    DoubleClick = false -- Requires double-click for risky actions
 })
