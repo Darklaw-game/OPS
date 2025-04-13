5d4c875362d6b6b08dea7ca18c9acc6f58e93417
Darklaw-game <darklaw.game@gmail.com>
2025-04-13 11:18:45 -0500

    Update AstralV3.lua

--- AstralV3.lua
+++ AstralV3.lua
@@ -8,7 +8,7 @@ local Window = Library:CreateWindow({
     AutoShow = true
 })
 
-local MainTab = Window:AddTab("Main", "home") -- Second parameter is the icon name (optional)
+
 local StatsTab = Window:AddTab("stats", "home") -- Second parameter is the icon name (optional)
 
 local LeftGroupbox = MainTab:AddLeftGroupbox("hubs")
