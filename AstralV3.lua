1bda2ae5d52883051c8a81808258354fc8b3879b
Darklaw-game <darklaw.game@gmail.com>
2025-04-13 21:34:32 -0500

    Update AstralV3.lua

--- AstralV3.lua
+++ AstralV3.lua
@@ -117,15 +117,3 @@ local TeleTab = Window:AddTab("Teleports", "arrow-left-right") -- Second paramet
 local LeftGroupbox = MainTab:AddLeftGroupbox("Ingame")
 local RightGroupbox = MainTab:AddRightGroupbox("OutGame")
 
-local Button = LeftGroupBox:AddButton({
-        Text = "lobby"
-        Func = Function()
-            local player = game:GetService("Players").LocalPlayer  
-local character = player.Character or player.CharacterAdded:Wait()  
-local humanoidRootPart = character:FindFirstChild("HumanoidRootPart")  
-
-if humanoidRootPart then  
-    humanoidRootPart.CFrame = CFrame.new(-897.2923583984375, 327.8538818359375, -2.0450494289398193)  
-end
-        end,
-    })
